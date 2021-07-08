6c1454e3ea759763311bb37a45d52ff8fe15c19a
Veronica <petersonv2913@gmail.com>
2021-07-08 10:26:54 -0400

    added jbuilder gem.

--- app/controllers/v1/sessions_controller.rb
+++ app/controllers/v1/sessions_controller.rb
@@ -3,7 +3,7 @@ class V1::SessionsController < ApplicationController
         user = User.where(email: params[:email]).first
         #Devise's valid_password method checks the password is correct then JSON is returned
         if user && user.valid_password?(params[:password])
-            render json: user.as_json(only: [:email]), status: :created
+            render json: user.as_json(only: [:email, :authentication_token]), status: :created
         else
             #if user is not authorized, the header will return unauthorized
             head(:unauthorized)
